aa17c3281b020417b2f18635660a505f8af94aee
Melissa Perez <melmperz@gmail.com>
2022-08-16 15:55:57 -0400

    Finish project

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/App'
 import reportWebVitals from './reportWebVitals';
-// registry.npmjs.org/-/v1/search?text=react
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
